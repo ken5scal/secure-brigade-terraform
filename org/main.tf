0c8208a5394f7697e86f7e549a323339f3d27f52
Kengo Suzuki <kengoscal@gmail.com>
2019-12-14 17:12:11 +0900

    fix

---
+++ org/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region     = "ap-northeast-1"
+  profile    = "791325445011_AdministratorAccess" // aws(master) profile in ~/.aws/credentials
+}
+
+resource "aws_organizations_account" "test" {
+  email = "kengoscal+aws-test@gmail.com"
+  name  = "secure-brigade-test"
+  role_name = "myOrganizationRole"
+}
+
+resource "aws_organizations_account" "security" {
+  email = "kengoscal+aws-security@gmail.com"
+  name  = "secure-brigade-security"
+}
+
+resource "aws_organizations_account" "compliance" {
+  email = "kengoscal+aws-compliance@gmail.com"
+  name  = "secure-brigade-compliance"
+}
+
+resource "aws_organizations_account" "logging" {
+  email = "kengoscal+aws-logging@gmail.com"
+  name  = "secure-brigade-logging"
+}
+
+resource "aws_organizations_account" "shared-resources" {
+  email = "kengoscal+aws-shared-resources@gmail.com"
+  name  = "secure-brigade-shared-resources"
+}
+
+resource "aws_organizations_account" "prod" {
+  email = "kengoscal+aws-prod@gmail.com"
+  name  = "secure-brigade-prod"
+}
+
+resource "aws_organizations_account" "stg" {
+  email = "kengoscal+aws-stg@gmail.com"
+  name  = "secure-brigade-stg"
+}
+
+resource "aws_organizations_account" "sandbox" {
+  email = "kengoscal+aws-sandbox@gmail.com"
+  name  = "secure-brigade-sandbox"
+}
+

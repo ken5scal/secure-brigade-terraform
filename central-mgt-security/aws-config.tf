4e513aa84063777782d33d6ae5e71fe691928475
Kengo Suzuki <kengoscal@gmail.com>
2019-12-27 02:07:15 +0900

    update

--- central-mgt-security/aws-config.tf
+++ central-mgt-security/aws-config.tf
@@ -3,7 +3,42 @@
 
 resource "aws_s3_bucket" "config-bucket" {
   provider = aws.shared-resources
+  region   = var.region
   bucket   = "aws-config-bucket-for-secure-brigade"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = aws_kms_key.config-bucket.key_id
+      }
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+
+  tags = {
+    name   = "aws-config-bucket"
+    env    = "shared-resources"
+    source = "AWS Config"
+    jobs   = "config-mgt"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "config-bucket" {
@@ -12,6 +47,7 @@ resource "aws_s3_bucket_public_access_block" "config-bucket" {
   block_public_acls   = true
   block_public_policy = true
 }
+
 
 resource "aws_s3_bucket_policy" "config-bucket" {
   provider = aws.shared-resources
@@ -48,7 +84,13 @@ resource "aws_s3_bucket_policy" "config-bucket" {
 POLICY
 }
 
-//module "config" {
-//  source = "./aws-config"
-//  config-bueckt-name = aws_s3_bucket.config-bucket.bucket
-//}
+resource "aws_kms_key" "config-bucket" {
+  provider    = aws.shared-resources
+  description = "key to encrypt/decrypt s3 storing AWS Configs"
+}
+
+resource "aws_kms_alias" "config-bucket" {
+  provider      = aws.shared-resources
+  name          = "alias/config-bucket-key"
+  target_key_id = aws_kms_key.config-bucket.key_id
+}

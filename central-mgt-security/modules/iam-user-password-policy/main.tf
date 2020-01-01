664176274edc4195751fb5f1abd03dabdb62d426
Kengo Suzuki <kengoscal@gmail.com>
2020-01-02 02:50:34 +0900

    change password policy

--- central-mgt-security/modules/iam-user-password-policy/main.tf
+++ central-mgt-security/modules/iam-user-password-policy/main.tf
@@ -5,6 +5,6 @@ resource "aws_iam_account_password_policy" "strict" {
   require_uppercase_characters   = true
   require_symbols                = true
   allow_users_to_change_password = true
-  password_reuse_prevention      = 20
+  password_reuse_prevention      = 24
   max_password_age               = 30
 }

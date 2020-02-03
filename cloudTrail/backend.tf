195da71e745d1e3f15ad98e51c189cab44d63262
Kengo Suzuki <kengoscal@gmail.com>
2020-02-04 08:59:56 +0900

    change backend setting file

--- cloudTrail/backend.tf
+++ cloudTrail/backend.tf
@@ -9,7 +9,7 @@ terraform {
     // aws(shared-resources) profile in ~/.aws/credentials
     // profile is not required as long as AWS ENV is set.
     bucket       = "terraform-backend-secure-brigade"
-    key          = "${var.application}/${var.environment}.tfstate"
+    key          = "cloudTrail/terraform.tfstate"
     kms_key_id   = "arn:aws:kms:ap-northeast-1:584423914806:key/ffca358f-a093-42ce-a2de-45d16c0a9610"
     encrypt      = true
     region       = "ap-northeast-1"

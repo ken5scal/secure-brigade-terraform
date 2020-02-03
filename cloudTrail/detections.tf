97c7270e4078995b0c0c2ba127b2329e8cb9eca7
Kengo Suzuki <kengoscal@gmail.com>
2020-02-03 23:12:39 +0900

    add new cloud metrics

--- cloudTrail/detections.tf
+++ cloudTrail/detections.tf
@@ -11,10 +11,39 @@ resource "aws_cloudwatch_log_metric_filter" "root-account-usage-detection" {
   }
 }
 
+resource "aws_cloudwatch_log_metric_filter" "unauthorized-api-usage-detection" {
+  name           = "UnauthorizedAPIUsageDetection"
+  log_group_name = aws_cloudwatch_log_group.cloudtrail.name
+  pattern        = "{($.errorCode=\"*UnauthorizedOperation\") || ($.errorCode=\"AccessDenied*\")}"
+
+  metric_transformation {
+    name          = "UnauthorizedAPIUsageDetection"
+    namespace     = "AwsCISBenchmark"
+    value         = 1
+    default_value = 0
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "root-account-usage-detection" {
   alarm_name          = "AWS-CIS-1.1-RootAccountUsage"
   namespace           = aws_cloudwatch_log_metric_filter.root-account-usage-detection.metric_transformation[0].namespace
   metric_name         = aws_cloudwatch_log_metric_filter.root-account-usage-detection.name
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  period              = 300
+  evaluation_periods  = 1
+  threshold           = 1
+  datapoints_to_alarm = 1
+  statistic           = "Sum"
+  alarm_actions = [
+  aws_sns_topic.root-account-usage-detection.arn]
+  ok_actions = [
+  aws_sns_topic.root-account-usage-detection.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "unauthorized-api-usage-detection" {
+  alarm_name          = "AWS-CIS-3.1-UnauthorizedAPIUsage"
+  namespace           = aws_cloudwatch_log_metric_filter.unauthorized-api-usage-detection.metric_transformation[0].namespace
+  metric_name         = aws_cloudwatch_log_metric_filter.unauthorized-api-usage-detection.name
   comparison_operator = "GreaterThanOrEqualToThreshold"
   period              = 300
   evaluation_periods  = 1

0ba14b036eb4d332a7a52d77ce8c252d54920170
Kengo Suzuki <kengoscal@gmail.com>
2020-02-03 23:52:25 +0900

    add cis benchmark cloudwatch 3.1

--- cloudTrail/detections.tf
+++ cloudTrail/detections.tf
@@ -1,67 +1,27 @@
-resource "aws_cloudwatch_log_metric_filter" "root-account-usage-detection" {
-  name           = "RootAccountUsageCount"
-  log_group_name = aws_cloudwatch_log_group.cloudtrail.name
-  pattern        = "{$.userIdentity.type = \"Root\" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != \"AwsServiceEvent\"}"
-
-  metric_transformation {
-    name          = "RootAccountUsageCount"
-    namespace     = "AwsCISBenchmark"
-    value         = 1
-    default_value = 0
-  }
+module "root-account-usage-detection" {
+  source                     = "./modules/cisAlarms"
+  cis-name                   = "AWS-CIS-1.1-RootAccountUsage"
+  cloudwatch-log-group-name  = aws_cloudwatch_log_group.cloudtrail.name
+  pattern                    = "{$.userIdentity.type = \"Root\" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != \"AwsServiceEvent\"}"
+  sns-topic-alarm-action-arn = aws_sns_topic.aws-cis-benchmark.arn
+  sns-topic-ok-action-arn    = aws_sns_topic.aws-cis-benchmark.arn
 }
 
-resource "aws_cloudwatch_log_metric_filter" "unauthorized-api-usage-detection" {
-  name           = "UnauthorizedAPIUsageDetection"
-  log_group_name = aws_cloudwatch_log_group.cloudtrail.name
-  pattern        = "{($.errorCode=\"*UnauthorizedOperation\") || ($.errorCode=\"AccessDenied*\")}"
-
-  metric_transformation {
-    name          = "UnauthorizedAPIUsageDetection"
-    namespace     = "AwsCISBenchmark"
-    value         = 1
-    default_value = 0
-  }
+module "unauthorized-api-usage-detection" {
+  source                     = "./modules/cisAlarms"
+  cis-name                   = "AWS-CIS-3.1-UnauthorizedAPIUsage"
+  cloudwatch-log-group-name  = aws_cloudwatch_log_group.cloudtrail.name
+  pattern                    = "{($.errorCode=\"*UnauthorizedOperation\") || ($.errorCode=\"AccessDenied*\")}"
+  sns-topic-alarm-action-arn = aws_sns_topic.aws-cis-benchmark.arn
+  sns-topic-ok-action-arn    = aws_sns_topic.aws-cis-benchmark.arn
 }
 
-resource "aws_cloudwatch_metric_alarm" "root-account-usage-detection" {
-  alarm_name          = "AWS-CIS-1.1-RootAccountUsage"
-  namespace           = aws_cloudwatch_log_metric_filter.root-account-usage-detection.metric_transformation[0].namespace
-  metric_name         = aws_cloudwatch_log_metric_filter.root-account-usage-detection.name
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  period              = 300
-  evaluation_periods  = 1
-  threshold           = 1
-  datapoints_to_alarm = 1
-  statistic           = "Sum"
-  alarm_actions = [
-  aws_sns_topic.root-account-usage-detection.arn]
-  ok_actions = [
-  aws_sns_topic.root-account-usage-detection.arn]
+resource "aws_sns_topic" "aws-cis-benchmark" {
+  name = "AwsCISBenchmarkAlarm"
 }
 
-resource "aws_cloudwatch_metric_alarm" "unauthorized-api-usage-detection" {
-  alarm_name          = "AWS-CIS-3.1-UnauthorizedAPIUsage"
-  namespace           = aws_cloudwatch_log_metric_filter.unauthorized-api-usage-detection.metric_transformation[0].namespace
-  metric_name         = aws_cloudwatch_log_metric_filter.unauthorized-api-usage-detection.name
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  period              = 300
-  evaluation_periods  = 1
-  threshold           = 1
-  datapoints_to_alarm = 1
-  statistic           = "Sum"
-  alarm_actions = [
-  aws_sns_topic.root-account-usage-detection.arn]
-  ok_actions = [
-  aws_sns_topic.root-account-usage-detection.arn]
-}
-
-resource "aws_sns_topic" "root-account-usage-detection" {
-  name = "rootAccountUsageDetection"
-}
-
-resource "aws_sns_topic_subscription" "root-account-usage-detection" {
-  topic_arn = aws_sns_topic.root-account-usage-detection.arn
+resource "aws_sns_topic_subscription" "aws-cis-benchmark" {
+  topic_arn = aws_sns_topic.aws-cis-benchmark.arn
   protocol  = "https"
   // AWS Chatbot is created manually because terraform is not ready yet
   endpoint               = "https://global.sns-api.chatbot.amazonaws.com"
---
+++ cloudTrail/modules/cisAlarms/main.tf
@@ -0,0 +1,32 @@
+variable cis-name {}
+variable cloudwatch-log-group-name {}
+variable pattern {}
+variable sns-topic-alarm-action-arn {}
+variable sns-topic-ok-action-arn {}
+
+resource "aws_cloudwatch_log_metric_filter" "metric-filter" {
+  name           = var.cis-name
+  log_group_name = var.cloudwatch-log-group-name
+  pattern        = var.pattern
+
+  metric_transformation {
+    name          = var.cis-name
+    namespace     = "AwsCISBenchmark"
+    value         = 1
+    default_value = 0
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm" {
+  alarm_name          = var.cis-name
+  namespace           = aws_cloudwatch_log_metric_filter.metric-filter.metric_transformation[0].namespace
+  metric_name         = aws_cloudwatch_log_metric_filter.metric-filter.name
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  period              = 300
+  evaluation_periods  = 1
+  threshold           = 1
+  datapoints_to_alarm = 1
+  statistic           = "Sum"
+  alarm_actions       = [var.sns-topic-alarm-action-arn]
+  ok_actions          = [var.sns-topic-ok-action-arn]
+}

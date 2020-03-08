2db3e9be62489f73c33b666a9284ca5bcfb19f47
Kengo Suzuki <kengoscal@gmail.com>
2020-03-08 23:55:14 +0900

    update

--- org/main.tf
+++ org/main.tf
@@ -2,6 +2,23 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+    "sso.amazonaws.com",
+    "ssm.amazonaws.com",
+    "tagpolicies.tag.amazonaws.com"
+  ]
+
+  enabled_policy_types = [
+    "SERVICE_CONTROL_POLICY",
+    "TAG_POLICY"
+  ]
+
+  feature_set = "ALL"
+}
+
 resource "aws_organizations_account" "security" {
   email = "kengoscal+aws-security@gmail.com"
   name  = "secure-brigade-security"
---
+++ org/org_policy.tf
@@ -0,0 +1,39 @@
+resource "aws_organizations_policy" "deny-deleting-security-and-audit-settings" {
+  name    = "DenyDeletingSecurityAndAuditSettings"
+  content = <<CONTENT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "DenyDeletingSecurityAndAuditSettings",
+            "Effect": "Deny",
+            "Action": [
+                "guardduty:Delete*",
+                "guardduty:DisassociateFromMasterAccount",
+                "guardduty:UntagResource",
+                "guardduty:DeclineInvitations",
+                "guardduty:DisassociateMembers",
+                "cloudtrail:DeleteTrail",
+                "cloudtrail:RemoveTags",
+                "cloudtrail:StopLogging",
+                "securityhub:DeclineInvitations",
+                "securityhub:Delete*",
+                "securityhub:Disassociate*",
+                "securityhub:Disable*",
+                "securityhub:UntagResource",
+                "config:Delete*",
+                "config:StopConfigurationRecorder"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+CONTENT
+}
+
+resource "aws_organizations_policy_attachment" "root" {
+  policy_id = aws_organizations_policy.deny-deleting-security-and-audit-settings.id
+  target_id = aws_organizations_organization.org.roots.0.id
+}

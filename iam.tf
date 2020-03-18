794fd5d147d276a32f3b52d59224183b354447aa
Kengo Suzuki <kengoscal@gmail.com>
2020-03-19 02:06:35 +0900

    add azure sentinel connector

--- iam.tf
+++ iam.tf
@@ -1,3 +1,76 @@
+resource "aws_iam_role" "cloudtrail-servie" {
+  name               = "SecureBrigadeCloudTrailServiceRole"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy" "cloudtrail-to-cw-log" {
+  role   = aws_iam_role.cloudtrail-servie.name
+  name   = "SendCloudTrailLogToCloudWatchLogPolicy"
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:log-group:CloudTrailLogs:log-stream:*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role" "azure-sentinel" {
+  name = "AzureSentinelRole"
+  // https://docs.microsoft.com/en-us/azure/sentinel/connect-aws
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::197857026523:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "3c38da68-f848-4cfe-a698-0d61050d3be2"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "azure-sentinel" {
+  role       = aws_iam_role.azure-sentinel.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCloudTrailReadOnlyAccess"
+}
+
+// -------------------------------------
+// Terraform Role in each AWS account
+// -------------------------------------
 module "terraform-administrator-in-compliance" {
   providers = {
     aws = aws.compliance
@@ -46,47 +119,9 @@ module "terraform-administrator-in-security" {
   iam-policy-document      = data.aws_iam_policy.administrator-access.policy
 }
 
-resource "aws_iam_role" "cloudtrail-servie" {
-  name               = "SecureBrigadeCloudTrailServiceRole"
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "cloudtrail.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy" "cloudtrail-to-cw-log" {
-  role   = aws_iam_role.cloudtrail-servie.name
-  name   = "SendCloudTrailLogToCloudWatchLogPolicy"
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "arn:aws:logs:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:log-group:CloudTrailLogs:log-stream:*"
-            ]
-        }
-    ]
-}
-POLICY
-}
-
+// --------------------
+// Password Policy
+// --------------------
 module "password-policy-master" {
   source = "./modules/iam-user-password-policy"
 }

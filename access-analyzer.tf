0a6c1d9ae3267946e3910b62106fd7f8ac2b8121
Kengo Suzuki <kengoscal@gmail.com>
2020-04-05 19:09:40 +0900

    add detective resources

---
+++ access-analyzer.tf
@@ -0,0 +1,2 @@
+// terraform resources for access analyzer is not yet ready for organizaiton (and delegation)
+// I turned on them by GUI
---
+++ detective.tf
@@ -0,0 +1,8 @@
+// manually turned on using https://github.com/aws-samples/amazon-detective-multiaccount-scripts
+//% python3 enableDetective.py --master_account 085773780922 --assume_role TerraformAdministrativeRole --enabled_regions ap-northeast-1 --input_file hoge.csv                                                                                                                   (git)-[master]
+//cat hoge.csv
+//791325445011,kengoscal+aws-master@gmail.com
+//491027160565,kengoscal+aws-compliance@gmail.com
+//297323088823,kengoscal+aws-stg@gmail.com
+//806884417180,kengoscal+aws-prod@gmail.com
+//085773780922,kengoscal+aws-security@gmail.com
